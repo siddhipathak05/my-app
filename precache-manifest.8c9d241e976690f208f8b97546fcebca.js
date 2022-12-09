89cfa3413d060ca30f657dd4c21efa010414d827
siddhipathak05 <spathak2910@gmail.com>
2022-12-09 13:16:09 +0530

    Updates

---
+++ precache-manifest.8c9d241e976690f208f8b97546fcebca.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "287e381e26342e1e1ba9",
+    "url": "/my-app/static/js/main.287e381e.chunk.js"
+  },
+  {
+    "revision": "5970b8146f719c92d862",
+    "url": "/my-app/static/js/1.5970b814.chunk.js"
+  },
+  {
+    "revision": "a3bd857c0333674b1d75",
+    "url": "/my-app/static/js/runtime~main.a3bd857c.js"
+  },
+  {
+    "revision": "cb133a827d35771a44ab22614e9c94ca",
+    "url": "/my-app/index.html"
+  }
+];
